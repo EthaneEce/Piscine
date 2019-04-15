a5c2839495a44d00199fba8f55d6f30363153a40
NabilChebili <nabilchebili@ymail.com>
2019-04-15 11:13:41 +0200

    initialisation allegro

--- main.cpp
+++ main.cpp
@@ -1,9 +1,57 @@
 #include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <allegro.h>
+#include <time.h>
 
-int main()
+
+
+/**int main()
 {
-
+    /// initialisation générale
+    if (allegro_init() != 0)
+        return 1;
+    /// pour utiliser le clavier
+    install_keyboard();
+    /// pour utiliser la souris
+    install_mouse();
+    /// initialisation du MODE GRAPHIQUE
+    //on fixe le mode couleur (8, 16, 24 ou 32 bits)
+    set_color_depth(desktop_color_depth());
+    // on essaie d’ouvrir une fenêtre de largeur*hauteur pixels
+    // si l’ouverture échoue on quitte
+    if (set_gfx_mode(GFX_AUTODETECT_WINDOWED,200,200,0,0)!=0)
+    {
+        allegro_message("probleme mode graphique : %s", allegro_error);
+        allegro_exit();
+        exit(EXIT_FAILURE);
+    }
+    /// si on veut afficher le pointeur de souris
+    show_mouse(screen);
 
     std::cout << "Hello world!" << std::endl;
     return 0;
-}
+}**/
+
+int main()
+{
+/* Initialisation d’Allegro */
+if (allegro_init() != 0)
+return 1 ;
+/* Affichage de votre premier
+message sous Allegro */
+allegro_message("hello world") ;
+
+install_keyboard();
+    install_mouse();
+
+    set_color_depth(desktop_color_depth());
+    if (set_gfx_mode(GFX_AUTODETECT_FULLSCREEN,800,600,0,0)!=0)
+    {
+        allegro_message("prb gfx mode");
+        allegro_exit();
+        exit(EXIT_FAILURE);
+    }
+    show_mouse(screen);
+return 0 ;
+} END_OF_MAIN() ;
