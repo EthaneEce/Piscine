bd83dbf395ff2263bad34a9631ba43793e9b413c
Walid <walid.jabari@outlook.com>
2019-04-16 15:09:03 +0200

    Update Sommet.cpp

--- Sommet.cpp
+++ Sommet.cpp
@@ -1,28 +1,28 @@
 #include "sommet.h"
 
-Sommet::Sommet(int id,double x,double y):m_id{id},m_x{x},m_y{y}
+Sommet::Sommet ( int id , double x , double y ) :m_id { id } , m_x { x } , m_y { y }
 {}
 
-int Sommet::getid()const
+int Sommet::getid ( )const
 {
     return m_id;
 }
 
-double Sommet::gety()const
+double Sommet::gety ( )const
 {
     return m_x;
 }
 
-double Sommet::getx()const
+double Sommet::getx ( )const
 {
     return m_y;
 }
 
-void Sommet::afficherData() const{
- std::cout<<"    "<<m_id<<" : "<<"(x,y)=("<<m_x<<","<<m_y<<")"<<std::endl;
+void Sommet::afficherData ( ) const {
+    std::cout << "    " << m_id << " : " << "(x,y)=(" << m_x << "," << m_y << ")" << std::endl;
 }
 
-Sommet::~Sommet()
+Sommet::~Sommet ( )
 {
     //dtor
 }
