96d42ca6655ef19944949ea5918a9de331370b81
Walid <walid.jabari@outlook.com>
2019-04-18 01:06:25 +0200

    Initial Commit
    Branche pour tester des fonctions avec des valeurs "fournies" si elle dépend de la complétion d'une autre fonction.

--- Timer.h
+++ Timer.h
@@ -1,6 +1,8 @@
 #pragma once
 #include <chrono>
 #include <iostream>
+
+
 class Timer
 {
 private:
--- main.cpp
+++ main.cpp
@@ -2,14 +2,31 @@
 #include "Graphe.h"
 
 
+std::ostream& operator<<( std::ostream& out , std::vector<float> vec )
+{
+    for ( auto& a : vec )
+        out << a << " ";
+    return out;
+}
 int main ( )
 {
-    Graphe b ( "txt/triville.txt" , "txt/triville_weights_0.txt" );
-    std::cin.get ( );
+    Graphe b ( "txt/broadway.txt" , "txt/broadway_weights_0.txt" );
+    std::vector<bool> vec1 { false, true, false, true, true };
+    std::vector<bool> vec2 { false, true, true, false, true };
+    std::vector<bool> vec3 { false, true, true, true, false };
+    std::vector<bool> vec4 { true, false, false, true, true };
+    std::vector<bool> vec5 { true, false, true, false, true };
+    std::vector<bool> vec6 { true, false, true, true, false };
+    std::vector<bool> vec7 { true, true, false, false, true };
+    std::vector<bool> vec8 { true, true, false, true, false };
 
-    auto sol = b.Kruskal ( );
-    Graphe g ( b.getsommets ( ) , sol );
-    auto vec = g.poidsTotaux ( );
+    std::vector<std::vector<bool>>vec { vec1 ,
+                vec2 , vec3 , vec4 ,
+                vec5 , vec6 , vec7 , vec8 };
+
+    auto par = b.Pareto ( vec );
+    for ( auto& a : par )
+        std::cout << a->poidsTotaux ( ) << std::endl;
     std::cin.get ( );
 }
 
