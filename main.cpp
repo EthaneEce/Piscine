377f97d5a6cf2c7a10dc621c38ba7a09f5e630d0
Walid <walid.jabari@outlook.com>
2019-04-19 00:06:09 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -13,21 +13,8 @@ int main ( )
 {
     std::ifstream ifs ( "fichier.txt" );
     Graphe b ( "txt/manhattan.txt" , "txt/manhattan_weights_0.txt" );
-    size_t size1 , size2;
-    ifs >> size1 >> size2;
-    std::vector<std::vector<bool>> BruteForce_Manhattan;
-    for ( size_t i = 0; i < size1; i++ )
-    {
-        std::vector<bool> temp;
-        for ( size_t j = 0; j < size2; j++ )
-        {
-            bool a;
-            ifs >> a;
-            temp.push_back ( a );
-        }
-        BruteForce_Manhattan.push_back ( temp );
-    }
-    auto par = b.Pareto ( BruteForce_Manhattan );
+
+    auto par = b.Pareto ( b.bruteforce ( ) );
     std::cout << par.size ( ) << std::endl;
     for ( auto& a : par )
         std::cout << a->poidsTotaux ( ) << std::endl;
