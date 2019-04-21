48f991685c38f5416b8cfd4254373775cf4b0116
EthaneEce <46593602+EthaneEce@users.noreply.github.com>
2019-04-21 14:31:26 +0200

    Commentaires

--- graphisme.h
+++ graphisme.h
@@ -10,9 +10,9 @@
 #include "Graphe.h"
 
 int draw_bouton(int posx, int posy, int pos2x, int pos2y, int color1, int color2, int profondeur, char* texte,FONT* font1,BITMAP* buffer);
-int menu(BITMAP* buffer, Graphe& a, FONT* font1,FONT* titre);
-void choixPoids(BITMAP* buffer,BITMAP* fond, FONT* font1,FONT*titre,std::string fichier1, Graphe& b,int nbPoids);
-void choixUtilisationGraph(BITMAP* buffer, BITMAP* fond, FONT* font1,FONT* titre, Graphe& b,std::string nom);
+int menu(BITMAP* buffer, Graphe& a, FONT* font1,FONT* titre);                                                     //Choix du graphe dans le dossier txt. On rentre dans les sous programmes qui suivent en "cascade"
+void choixPoids(BITMAP* buffer,BITMAP* fond, FONT* font1,FONT*titre,std::string fichier1, Graphe& b,int nbPoids); //Sélection du fichier poids correspondant à la ville choisie
+void choixUtilisationGraph(BITMAP* buffer, BITMAP* fond, FONT* font1,FONT* titre, Graphe& b,std::string nom);     //Sélection de l'utilisation du graphe. On va appeler les différentes méthodes de Graphe pour les dessiner
 void dessinerBrut(BITMAP*buffer,Graphe b,std::vector<std::vector<bool>> Ttgraphes,double x, double y,int proportion);
 void dessinerPareto ( BITMAP * buffer , Graphe b , double x , double y , std::vector<std::vector<bool>> Ttgraphes , std::vector<std::vector<bool>> Ttgraphes2 , std::vector<Graphe*> Pareto , bool dess, bool pareto2 );
 
